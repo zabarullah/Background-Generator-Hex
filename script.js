687aa1bc998af4f1f6cee2f8f6711f008219d414
zabarullah <60830173+zabarullah@users.noreply.github.com>
2021-02-15 11:33:55 +0000

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
 var randomButton = document.getElementById("random");
-var randomResult = document.getElementById("randonResult")
 
 function setGradient() {
 	body.style.background = "linear-gradient(to right, " + color1.value + "," + color2.value + ")";
